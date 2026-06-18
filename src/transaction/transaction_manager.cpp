295b93ab42b78ef6abd19e5b2505e5a40cc69f45
lullabyeoytl <2199288762@qq.com>
2026-06-18 10:46:07 +0800

    feat(transaction_manager): ensure log buffer is flushed to disk during commit

--- src/transaction/transaction_manager.cpp
+++ src/transaction/transaction_manager.cpp
@@ -201,6 +201,7 @@ void TransactionManager::commit(Transaction *txn, LogManager *log_manager) {
         CommitLogRecord log_record(txn_id);
         log_record.prev_lsn_ = txn->get_prev_lsn();
         lsn_t lsn = log_manager->add_log_to_buffer(&log_record);
+        log_manager->flush_buffer_to_disk_safe();
         txn->set_prev_lsn(lsn);
     }
 
